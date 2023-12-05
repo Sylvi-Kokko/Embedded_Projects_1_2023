8039c5791421956c66a8e1ef9bc9f72ad15604b1
Sylvi Kokko <sylvi.kokko@tuni.fi>
2023-12-05 12:46:15 +0200

    Almost finished with commenting

--- final_embedded/final_embedded.ino
+++ final_embedded/final_embedded.ino
@@ -23,25 +23,25 @@ const int rs = 52, en = 53, d4 = 50, d5 = 51, d6 = 49, d7 = 48;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int analogPin1 = A0;   
 int analogPin2 = A1;
-int pwm_R, pwm_L, val, y_akseli, x_pwm, dir_L, dir_R, bigpulsecountright, bigpulsecountleft;  
+int pwm_R, pwm_L, val, y_pwm, x_pwm, dir_L, dir_R, bigpulsecountright, bigpulsecountleft;  
 int st_Y = 503;
 int st_X = 496;
 int left_count = 0;
 int right_count = 0;
 const byte buttonPin = 19;
-bool isOn = true;
+bool steering_mode = true;
 float x_akseli, val1, val2;
 
 void buttonPressed(){ 
-  if (isOn){
-    isOn = false;
+  if (steering_mode){
+    steering_mode = false;
   }
-  else if (!isOn){
-    isOn = true;
+  else if (!steering_mode){
+    steering_mode = true;
   }
 }
 
-void count_reset() {
+void count_reset() { //Count reset is called so that every new motion can start from 0
   left_count = 0;
   right_count = 0;
 }
@@ -82,7 +82,7 @@ return lcddegree;
 int left_turn(int cm){
   digitalWrite(Motor_L_dir_pin, Motor_return); 
   digitalWrite(Motor_R_dir_pin, Motor_forward); //Wheels rotate in opposite directions
-  while(right_count < (cm*1.4)){ //Move until count and inputted cm match up
+  while(right_count < (cm*1.4)){ //Move until count and inputed cm match up
     analogWrite(Motor_L_pwm_pin,255);
     analogWrite(Motor_R_pwm_pin,255);
   }
@@ -92,7 +92,7 @@ int left_turn(int cm){
   return 0;
 }
 
-int right_turn(int cm){
+int right_turn(int cm){ //As with left but opposite
   digitalWrite(Motor_R_dir_pin, Motor_return);
   digitalWrite(Motor_L_dir_pin, Motor_forward);
   while(right_count < cm * 1.4){
@@ -107,7 +107,7 @@ int right_turn(int cm){
 
 int go_straight(int cm){
   digitalWrite(Motor_L_dir_pin, Motor_forward); //Wheels move to the same direction
-  digitalWrite(Motor_R_dir_pin, Motor_forward);
+  digitalWrite(Motor_R_dir_pin, Motor_forward); //Otherwise same concept as left
   while(right_count < (cm*14)){
     analogWrite(Motor_L_pwm_pin,255);
     analogWrite(Motor_R_pwm_pin,255);
@@ -117,7 +117,7 @@ int go_straight(int cm){
   count_reset();
   return 0;
 }
-int go_back(int cm){
+int go_back(int cm){ 
   digitalWrite(Motor_L_dir_pin, Motor_return);
   digitalWrite(Motor_R_dir_pin, Motor_return);
   while(right_count < (cm*14)){
@@ -131,8 +131,7 @@ int go_back(int cm){
   return 0;
 }
 
-int turn_until(float target){
-  
+int turn_until(float target){ //
   int degree = 361.0;
   Wire.requestFrom(CMPS14_address, 1, true);
     Wire.beginTransmission(CMPS14_address);
@@ -151,14 +150,14 @@ int turn_until(float target){
     Serial.println("Wire not found");
   }
 
-  int angleDifference = target - degree;
+  int angleDifference = target - degree; //Calculate the shorter route to the asked for degree
   if (angleDifference > 180) {
     angleDifference -= 360;
   } else if (angleDifference < -180) {
     angleDifference += 360;
   }
 
-  if(angleDifference > 0){
+  if(angleDifference > 0){ //Set the direction of the motors according to the previous calculations 
     digitalWrite(Motor_L_dir_pin, Motor_return);
   digitalWrite(Motor_R_dir_pin, Motor_forward);
   }else{
@@ -166,7 +165,7 @@ int turn_until(float target){
   digitalWrite(Motor_R_dir_pin, Motor_return);
   }
 
-  while(!(degree <= target + 1 && degree >= target - 1)){
+  while(!(degree <= target + 1 && degree >= target - 1)){ //Continue to read the compass until target and read degree are +-1 from each other
     Serial.println(target);
     Serial.println(degree);
     analogWrite(Motor_L_pwm_pin,120);
@@ -189,41 +188,41 @@ int turn_until(float target){
     return 404;
   }
   }
-  analogWrite(Motor_L_pwm_pin,0);
+  analogWrite(Motor_L_pwm_pin,0); //Stop the motion
   analogWrite(Motor_R_pwm_pin,0);
-  count_reset();
+  count_reset(); 
   return 0;
 }
 
-void wifisteering(){
+void wifisteering(){ //Controlling the motion through wifi
  val = 0;
     lcd.setCursor(0, 0);
      if (Serial.available() > 0){
-    String message = Serial.readStringUntil('\n');
+    String message = Serial.readStringUntil('\n');//Read one line from serial
     Serial.print("Message received, content: ");
-    Serial.println(message);
+    Serial.println(message); 
     int pos_s;
     int poz_z;
     int movement = message.indexOf("Move");
     int turn = message.indexOf("Turn");
     int until = message.indexOf("UNTIL");
-    if (movement > -1){
+    if (movement > -1){ //If the command was movement, index will be bigger than -1
       Serial.println("Command = movement ");
       pos_s = message.indexOf(":");
-      if (pos_s > -1){
-        String stat = message.substring(pos_s + 1);
-        val = stat.toInt();
-        if(val<0){
+      if (pos_s > -1){ //If the index is bigger than -1 it exists
+        String stat = message.substring(pos_s + 1); // Get the string from after the ':'
+        val = stat.toInt(); //Get the int from it
+        if(val<0){ //Call the movement function with the correct distance
           go_back((-1)*val);
           
         }
           go_straight(val);
       }
     }
-    else if (turn > -1){
+    else if (turn > -1){ //If turn was called 
       Serial.println("Command = TURN ");
       pos_s = message.indexOf(":");
-      if (pos_s > -1){
+      if (pos_s > -1){ //Same as above but right or left turn is called
         String stat = message.substring(pos_s + 1);
         val = stat.toInt();
         if(val < 0){
@@ -231,13 +230,13 @@ void wifisteering(){
         }
           left_turn(val);
       }
-    }else if (until > -1){
+    }else if (until > -1){ //
       Serial.println("Command = UNTIL ");
       pos_s = message.indexOf(":");
-      if (pos_s > -1){
+      if (pos_s > -1){ //Same as above but turn until is called 
         String stat = message.substring(pos_s + 1);
         val = stat.toInt();
-        turn_until(val);
+        turn_until(val); 
       }
     }
     else{
@@ -246,22 +245,22 @@ void wifisteering(){
   }
 }
 
-void joysticksteering(){
+void joysticksteering(){ //Read the values from the joystick and move the wheels
     val1 = analogRead(analogPin2);
     val2 = analogRead(analogPin1);
-    val2 = val2 + 15.5;
+    val2 = val2 + 15.5; //The joystick is a bit off so these correct it to 0 
     val1 = val1 + 7.5;
-    x_pwm = map(val2, 0, 1023, -90, 90); //-100 to 100
-    y_akseli = map(val1, 0, 1023, -160, 160); //-100 to 100
-    if((val1 < 509.5)){
+    x_pwm = map(val2, 0, 1023, -90, 90);
+    y_pwm = map(val1, 0, 1023, -160, 160);
+    if((val1 < 509.5)){ //
       dir_L = 1;
       dir_R = 1;
       if(x_pwm < -1){
-        pwm_L = -1*x_pwm + abs(y_akseli);
-        pwm_R = x_pwm + abs(y_akseli);
+        pwm_L = -1*x_pwm + abs(y_pwm);
+        pwm_R = x_pwm + abs(y_pwm);
       }else{
-        pwm_L = -1*x_pwm + abs(y_akseli);
-        pwm_R = x_pwm + abs(y_akseli);
+        pwm_L = -1*x_pwm + abs(y_pwm);
+        pwm_R = x_pwm + abs(y_pwm);
       }
       
     }
@@ -269,11 +268,11 @@ void joysticksteering(){
       dir_L = 0;
       dir_R = 0; 
       if(x_pwm < -1){
-        pwm_L = -1*x_pwm + abs(y_akseli);
-        pwm_R = x_pwm + abs(y_akseli);
+        pwm_L = -1*x_pwm + abs(y_pwm);
+        pwm_R = x_pwm + abs(y_pwm);
       }else{
-        pwm_L = -1*x_pwm + abs(y_akseli);
-        pwm_R = x_pwm + abs(y_akseli);
+        pwm_L = -1*x_pwm + abs(y_pwm);
+        pwm_R = x_pwm + abs(y_pwm);
       }
     }else{
       if(x_pwm > 2){
@@ -335,7 +334,7 @@ void setup() {
 }
 
 void loop() {
-  if (isOn){
+  if (steering_mode){
     wifisteering();
     lcd.setCursor(0, 0);
     lcd.print("Steerviawifi/serial");
