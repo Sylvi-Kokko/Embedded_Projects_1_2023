642b574ec42999f5365c00da576222e51e87706f
Wilhelm Nilsson <hampus.nilsson@tuni.fi>
2024-04-24 11:28:31 +0300

    Encoder print to webpage

--- Embedded_Projects_2/final/final/final.ino
+++ Embedded_Projects_2/final/final/final.ino
@@ -59,7 +59,7 @@ struct RGB {
   int g;
   int b;
 };
-RGB colors[4]; //0 = red, 1 = blue, 2 = green, 3 = yellow
+RGB colors[4]; //0 = red, 1 = blue, 2 = green, 3 = yellow (Goal)
 int colDifTreshold = 80;
 DFRobot_TCS34725 tcs = DFRobot_TCS34725(&Wire, TCS34725_ADDRESS,TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 typedef struct RGB co;
@@ -95,6 +95,7 @@ for (int i = 0; i < numReadings; i++) {
     factor += LidarVals[i];
   }
     lidAv = factor/MAX_READINGS;
+    Serial2.println("Encoder=" + String(lidAv))
 }
 
 void buttonPressed(){
@@ -270,7 +271,6 @@ void setup() {
     Serial.println("No TCS34725 found ... check your connections");
     delay(1000);
   }
-  measure();
   for(int i = 0; i < 4; i++){
     while(digitalRead(19) == LOW){}
     colors[i] = RGBsensor();
